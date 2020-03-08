ed76c01a99d3c4da811ebe3d44098d3012df5033
OTree <kevin10410@gmail.com>
2020-03-08 21:24:36 +0800

    chore(package): implement babel to this project

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// can not use ES6 in this babel config
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
