0b8472107f24c1b9156be99d74f232ab6919ce23
Kira Yang <changru.yang02@gmail.com>
2020-03-11 15:38:36 +0800

    [modify] change test script

--- week1/jest.config.js
+++ week1/jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   // cacheDirectory: "/private/var/folders/s_/32k714x12q7cnh8ks1khp4_40000gn/T/jest_dx",
 
   // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+  // clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
