c9c538e76a52413d5483a417ce505138ca9d8557
Kira Yang <changru.yang02@gmail.com>
2020-03-11 14:13:19 +0800

    [feature] install babel & refactor from ES5 to ES6

--- week1/__test__/sum.test.js
+++ week1/__test__/sum.test.js
@@ -1,4 +1,5 @@
-const sum = require("../js/sum");
+// const sum = require("../js/sum");
+import sum from "../js/sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
--- week1/js/sum.js
+++ week1/js/sum.js
@@ -1,4 +1,9 @@
-function sum(a, b) {
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
+
+//ES6
+export default function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
