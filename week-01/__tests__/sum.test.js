f0e86d2e4a173ae6f59b36e55ed0aeabf86f849f
titangene <titangene.tw@gmail.com>
2020-03-12 18:14:46 +0800

    test(sum): test function sum

---
+++ week-01/__tests__/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('../src/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  // Arrange
+  let x = 1, y = 2;
+  let expected = 3;
+
+  // Act
+  let actual = sum(x, y);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
