e369e50a5d3ce053c16ebaf4b6a55af957b837eb
Penghua <kids5346@gmail.com>
2020-03-17 22:20:35 +0800

    test fucntion for matchers

--- week2-Ex/js/exercise.js
+++ week2-Ex/js/exercise.js
@@ -11,8 +11,23 @@ function getBooleanValue(num) {
   if(num > 5) return true;
 }
 
+function getString(str) {
+  return str.split('').reverse().join('');
+}
+
+function getArray(arr) {
+  return arr;
+}
+
+function throwError() {
+  throw new Error('you are using the wrong JDK');
+}
+
 export {
   sum,
   getObject,
   getBooleanValue,
+  getString,
+  getArray,
+  throwError,
 }
