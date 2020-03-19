dc2a7417b5b48055d6f05bdd51f6d6abb3f21d33
titangene <titangene.tw@gmail.com>
2020-03-19 15:30:08 +0800

    refactor(week-01): ES5 to ES6

---
+++ week-01/src/main.js
@@ -0,0 +1,6 @@
+// const sum = require('./sum');
+
+// ES6
+import sum from './sum';
+
+console.log(sum(1, 2));
--- week-01/src/sum.js
+++ week-01/src/sum.js
@@ -1,4 +1,8 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// ES6
+const sum = (a, b) => a + b;
+
+export default sum;
