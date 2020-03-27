d04997bfade047f421d223ae4d65132017bcb1ac
UnaLai <qqq323618@gmail.com>
2020-03-27 09:50:55 +0800

    [feat]:add week3 comment

--- week3/__test__/async.test.js
+++ week3/__test__/async.test.js
@@ -1,17 +1,19 @@
 const fetchData = require('../js/async');
 
 test('test fetchDataNotPromise', (done) => {
+  // 準備
   function callback(data) {
-    console.log("callback")
     try {
       // throw Error()
+      // 斷言
       expect(data).toBe('some data');
       done()
     } catch (e) {
       done(e);
     }
   }
-
+  
+  // 執行
   fetchData.fetchDataNotPromise(callback)
 });
 
@@ -59,10 +61,10 @@ test.skip('test fetchDataReturnPromise error with async/await', async () => {
 
 
 // async/await and matchers
-test('test fetchDataReturnPromise with matchers and async/await', async() => {
+test.skip('test fetchDataReturnPromise with matchers and async/await', async() => {
   await expect(fetchData.fetchDataReturnPromise()).resolves.toBe('some data')
 });
-test('test fetchDataReturnPromise error with matchers and async/await', async() => {
+test.skip('test fetchDataReturnPromise error with matchers and async/await', async() => {
   expect.assertions(1);
   await expect(fetchData.fetchDataReturnPromise()).rejects.toBe('something error')
 });
