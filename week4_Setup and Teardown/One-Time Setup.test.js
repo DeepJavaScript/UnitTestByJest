de6707c0e0d04d6e097a9ba6b4fc811c4849ff4e
letterliu <letter0206@gmail.com>
2020-04-09 00:21:29 +0800

    [feat]: One-Time Setup: beforeAll and afterAll

--- week4_Setup and Teardown/One-Time Setup.test.js
+++ week4_Setup and Teardown/One-Time Setup.test.js
@@ -7,12 +7,10 @@ afterAll(() => {
 });
 
 test('city database has Vienna', () => {
-  // expect('Vienna').toBeTruthy();
   console.log("Vienna");
 });
 
 test('city database has San Juan', () => {
-  // expect('San Juan').toBeTruthy();
   console.log("San Juan");
 });
 
